35f53a411c069a0678d882ea5cdfe49f70907e62
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-03-26 23:15:35 +0100

    adds create player, adds create game, adds signup link, adds method to return day from string

--- src/main/java/controllers/PlayerController.java
+++ src/main/java/controllers/PlayerController.java
@@ -35,18 +35,46 @@ public class PlayerController {
             List<Player> players = DBHelper.getAll(Player.class);
             String result = req.queryParams("day");
             Map<String, Object> model = new HashMap<>();
-//            List<Player> filteredPlayers = new ArrayList<>();
-//            for (Player player : players) {
-//                if (player.playerByDay(result)) {
-//                    filteredPlayers.add(player);
-//                }
-//            }
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            List<Player> filteredPlayers = new ArrayList<>();
+            for (Player player : players) {
+                if (player.playerByDay(result)) {
+                    filteredPlayers.add(player);
+                }
+            }
             model.put("day", result);
-//            model.put("filteredPlayers", filteredPlayers);
-            model.put("templates", "templates/Player/filter_by_day");
+            model.put("user", loggedInUser);
+            model.put("filteredPlayers", filteredPlayers);
+            model.put("templates", "templates/Player/filter_by_day.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
+        post("/players", (req, res) -> {
+            String username = req.queryParams("username");
+            String name = req.queryParams("name");
+            String location = req.queryParams("location");
+//            List<Day> days = new ArrayList<>();
+//            Collections.addAll(days, Day.values());
+            String availability[] = req.queryParams("availability").split(",");
+            List<Day> availableDays = new ArrayList<>();
+            for (String dayAvailable : availability) {
+                Day day = returnDayFromString(dayAvailable);
+                availableDays.add(day);
+            }
+            Player newPlayer = new Player(username, name, availableDays, location);
+            DBHelper.save(newPlayer);
+            res.redirect("/login");
+            return null;
+        });
+    }
 
+    public Day returnDayFromString(String dayAsString) {
+        Day foundDay = null;
+        for (Day day : Day.values()) {
+            if (day.getDay().equals(dayAsString)) {
+                foundDay = day;
+            }
+        }
+        return foundDay;
     }
 }
