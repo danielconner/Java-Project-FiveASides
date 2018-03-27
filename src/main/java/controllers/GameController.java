533b674d7c97755805252fcf11edec0cdd7b1005
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-27 14:25:18 +0100

    added crud to game class

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -20,6 +20,24 @@ public class GameController {
     }
 
     private void setupEndpoints() {
+
+        get("/games/:id/edit", (req, res) -> {
+            String stringGameId = req.params(":id");
+            Integer gameIntId = Integer.parseInt(stringGameId);
+            Game game = DBHelper.find(gameIntId, Game.class);
+            Map<String, Object> model = new HashMap<>();
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            List<Day> days = new ArrayList<>();
+            Collections.addAll(days, Day.values());
+            List<Venue> venues = DBHelper.getAll(Venue.class);
+            model.put("user", loggedInUser);
+            model.put("game", game);
+            model.put("venues", venues);
+            model.put("days", days);
+            model.put("template", "templates/Game/edit.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
         get("/games", (req, res) -> {
             List<Game> games = DBHelper.getAll(Game.class);
             Map<String, Object> model = new HashMap<>();
@@ -46,13 +64,13 @@ public class GameController {
         post("/games", (req, res) -> {
             String loggedInUser = LoginController.getLoggedInUserName(req, res);
             Player organiser = DBHelper.findByUsername(loggedInUser);
-            String name = req.queryParams("name");
+            String title = req.queryParams("title");
             int venueId = Integer.parseInt(req.queryParams("venue"));
             String time = req.queryParams("time");
             int numberOfRequiredPlayer = Integer.parseInt(req.queryParams("numberOfRequiredPlayer"));
             Day day = returnDayFromString(req.queryParams("day"));
             Venue venue = DBHelper.find(venueId, Venue.class);
-            Game newGame = new Game(name, venue, organiser, numberOfRequiredPlayer, day, time);
+            Game newGame = new Game(title, venue, organiser, numberOfRequiredPlayer, day, time);
             DBHelper.save(newGame);
             res.redirect("/games");
             return null;
@@ -63,7 +81,7 @@ public class GameController {
             Integer gameIntId = Integer.parseInt(stringGameId);
             Game game = DBHelper.find(gameIntId, Game.class);
             String title = game.getTitle();
-            Venue venue = game.getVenue();
+            Venue venue= game.getVenue();
             Player player = game.getOrganiser();
             Integer requiredPlayers = game.getNumberOfRequiredPlayer();
             List<Player> signedUpPlayers = DBHelper.playersPlaying(game);
@@ -73,6 +91,7 @@ public class GameController {
             Map<String, Object> model = new HashMap<>();
             String loggedInUser = LoginController.getLoggedInUserName(req, res);
             model.put("user", loggedInUser);
+            model.put("game", game);
             model.put("title", title);
             model.put("venue", venue);
             model.put("player", player);
@@ -83,6 +102,34 @@ public class GameController {
             model.put("time", time);
             model.put("template", "templates/Game/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post ("/games/:id/delete", (req, res) -> {
+            int id = Integer.parseInt(req.params(":id"));
+            Game gameToDelete = DBHelper.find(id, Game.class);
+            DBHelper.delete(gameToDelete);
+            res.redirect("/games");
+            return null;
+        }, new VelocityTemplateEngine());
+
+        post("/games/:id", (req, res) -> {
+            String stringGameId = req.params(":id");
+            Integer gameIntId = Integer.parseInt(stringGameId);
+            Game game = DBHelper.find(gameIntId, Game.class);
+            String title = req.queryParams("title");
+            int venueId = Integer.parseInt(req.queryParams("venue"));
+            String time = req.queryParams("time");
+            int numberOfRequiredPlayer = Integer.parseInt(req.queryParams("numberOfRequiredPlayer"));
+            Day day = returnDayFromString(req.queryParams("day"));
+            Venue venue = DBHelper.find(venueId, Venue.class);
+            game.setTitle(title);
+            game.setVenue(venue);
+            game.setNumberOfRequiredPlayer(numberOfRequiredPlayer);
+            game.setDay(day);
+            game.setTime(time);
+            DBHelper.save(game);
+            res.redirect("/games");
+            return null;
         }, new VelocityTemplateEngine());
 
     }
