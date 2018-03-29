8809d70c4afd077222cfa047441d53d7f3c29fef
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-29 11:32:49 +0100

    added heroku stuff

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -11,12 +11,14 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
 
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
         Seeds.seedData();
 
@@ -36,6 +38,13 @@ public class MainController {
             model.put("template","templates/main.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
 
